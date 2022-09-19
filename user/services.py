c4c451f76b05e3790d2b817d1f0192cffa4ba57f
hajin <f-a-k@naver.com>
2022-09-19 15:05:44 +0900

    docsting 내용 추가

--- user/services.py
+++ user/services.py
@@ -6,7 +6,7 @@ from user.serializers import UserSignupSerializer
 @transaction.atomic
 def post_user_signup_data(user_data: Dict) -> None:
     """
-    회원가입
+    회원가입 을 담당하는 service 입니다.
     """
     user_data_serializer = UserSignupSerializer(data=user_data)
     user_data_serializer.is_valid(raise_exception=True)
--- user/views.py
+++ user/views.py
@@ -13,7 +13,10 @@ class UserView(APIView):
     """
 
     def post(self, request: Request) -> Response:
-
+        """
+        post_user_signup_data 함수를 통해서 회원가입을 진행합니다.
+        validation 에 통과하지 못할 경우 validation 에러를 리턴 합니다.
+        """
         try:
             post_user_signup_data(request.data)
             return Response({"message": "회원가입을 성공하였습니다"}, status=status.HTTP_200_OK)
